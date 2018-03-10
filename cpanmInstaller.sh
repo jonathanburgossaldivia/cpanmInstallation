605cdca3a5b8beb58f4d53475ea07a959ee62eda
Jonathan Burgos Saldivia <jonathanburgoss@outlook.com>
2018-03-10 20:40:03 -0300

    Add files via upload

---
+++ cpanmInstaller.sh
@@ -0,0 +1,13 @@
+cpanm='https://cpanmin.us';
+text='[ $SHLVL -eq 1 ] && eval "$(perl -I$HOME/perl5/lib/perl5 -Mlocal::lib)"' ;
+wget -qO cpanm.pl "$cpanm" ||  curl -o cpanm.pl "$cpanm";
+perl cpanm.pl -l$HOME/perl5 App::cpanminus local::lib ;
+
+if [ -f .bashrc ]; then 
+	echo "$text" >>~/.bashrc;
+    source ~/.bashrc ;
+    else 
+    	echo "$text" >>~/.profile;
+    	source ~/.profile;
+fi;
+echo "END"
